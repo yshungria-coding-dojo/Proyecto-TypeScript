b89160eb1f64e2266f9fea865907268289e820ec
Yojan Hungría <yshungria.coding.dojo@gmail.com>
2023-09-03 14:06:16 -0500

    Modificaciones dinamicas ala HTML

--- main.ts
+++ main.ts
@@ -2,4 +2,16 @@ import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 
 export const ap = new Aprendiz('Yojan Stiben', 'Hungría', 'avatar.png' , 30, NivelEducativo.POSTGRADO );
 
-console.log(ap);
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
+    let tBodyAprendiz = document.createElement('tbody');
+    tBodyAprendiz.innerHTML = `<tr><td col><img src="./${aprendiz.avatar}" height=100></td></tr>
+    <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
+    <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
+    <tr><td>Nivel Educativo:</td colspan=2><td>${aprendiz.nivelEducativo}</td></tr>
+    <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`;
+    aprendizTable.appendChild(tBodyAprendiz);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 export var ap = new Aprendiz('Yojan Stiben', 'Hungría', 'avatar.png', 30, NivelEducativo.POSTGRADO);
-console.log(ap);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tBodyAprendiz = document.createElement('tbody');
+    tBodyAprendiz.innerHTML = "<tr><td col><img src=\"./".concat(aprendiz.avatar, "\" height=100></td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel Educativo:</td colspan=2><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    aprendizTable.appendChild(tBodyAprendiz);
+}
