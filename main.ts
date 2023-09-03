1bc3181cb103e586c4339e4a7d8ca54c89a2c8aa
Yojan Hungría <yshungria.coding.dojo@gmail.com>
2023-09-03 15:26:47 -0500

    Mostrando estadisticas de cursos

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+import { Curso } from "./curso.js";
 
-export const ap = new Aprendiz('Yojan Stiben', 'Hungría', 'avatar.png' , 30, NivelEducativo.POSTGRADO );
 
+let cursos = [new Curso("Python", 60, 95, true, 2020), new Curso("JavaScript", 30, 90, true, 2021), new Curso("POO en python", 10, 97, false, 2023)]
+
+export const ap = new Aprendiz('Yojan Stiben', 'Hungría', 'avatar.png' , 30, NivelEducativo.POSTGRADO, cursos);
+
+
+// Mostrar de manera dinánima el HTML
 let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+let estadisticaTable: HTMLElement = document.getElementById("estadisticas")!;
 
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
     let tBodyAprendiz = document.createElement('tbody');
@@ -14,4 +22,11 @@ function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
     <tr><td>Nivel Educativo:</td colspan=2><td>${aprendiz.nivelEducativo}</td></tr>
     <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`;
     aprendizTable.appendChild(tBodyAprendiz);
+}
+
+function mostrarEstadisticas(aprendiz: Aprendiz): void{
+    let numeroCertificados: number = aprendiz.darCursosCertificados();
+    let trElement: HTMLElement = document.createElement('tr');
+    trElement.innerHTML = `<td><b>Cursos certificados:</b></td><td>${numeroCertificados}</td>`;
+    estadisticaTable.appendChild(trElement);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,20 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
-export var ap = new Aprendiz('Yojan Stiben', 'Hungría', 'avatar.png', 30, NivelEducativo.POSTGRADO);
+import { Curso } from "./curso.js";
+var cursos = [new Curso("Python", 60, 95, true, 2020), new Curso("JavaScript", 30, 90, true, 2021), new Curso("POO en python", 10, 97, false, 2023)];
+export var ap = new Aprendiz('Yojan Stiben', 'Hungría', 'avatar.png', 30, NivelEducativo.POSTGRADO, cursos);
+// Mostrar de manera dinánima el HTML
 var aprendizTable = document.getElementById("aprendiz");
+var estadisticaTable = document.getElementById("estadisticas");
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 function mostrarDatosAprendiz(aprendiz) {
     var tBodyAprendiz = document.createElement('tbody');
     tBodyAprendiz.innerHTML = "<tr><td col><img src=\"./".concat(aprendiz.avatar, "\" height=100></td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel Educativo:</td colspan=2><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
     aprendizTable.appendChild(tBodyAprendiz);
+}
+function mostrarEstadisticas(aprendiz) {
+    var numeroCertificados = aprendiz.darCursosCertificados();
+    var trElement = document.createElement('tr');
+    trElement.innerHTML = "<td><b>Cursos certificados:</b></td><td>".concat(numeroCertificados, "</td>");
+    estadisticaTable.appendChild(trElement);
 }
